d3813fde2aecf7b155256d186f347ac4571bb11d
Lucas-Moreira-Rossani <131540771+Lucas-Moreira-Rossani@users.noreply.github.com>
2025-06-02 02:38:52 -0300

    Delete ia-cursos-online directory

--- ia-cursos-online/src/migrations.py
+++
@@ -1,7 +0,0 @@
-from flask_migrate import Migrate
-from src.main import app, db
-
-migrate = Migrate(app, db)
-
-if __name__ == '__main__':
-    app.run()
--- ia-cursos-online/src/wsgi.py
+++
@@ -1,5 +0,0 @@
-import os
-from src.main import app
-
-if __name__ == "__main__":
-    app.run()
